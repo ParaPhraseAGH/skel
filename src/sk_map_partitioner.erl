ae1cdb02bf8322f8b146fd315780c98d6b123c05
Marcin Miszczyk <mpmiszczyk@gmail.com>
2014-10-22 23:29:40 +0200

    Initial logic for map-pull workers.

--- src/sk_map_partitioner.erl
+++ src/sk_map_partitioner.erl
@@ -2,21 +2,21 @@
 %%% @author Sam Elliot <ashe@st-andrews.ac.uk>
 %%% @copyright 2012 University of St Andrews (See LICENCE)
 %%% @headerfile "skel.hrl"
-%%% 
+%%%
 %%% @doc This module contains the simple 'map' skeleton partitioner logic.
-%%% 
-%%% The Map skeleton is a parallel map. The skeleton applies a given function 
+%%%
+%%% The Map skeleton is a parallel map. The skeleton applies a given function
 %%% to the elements within one or more lists.
-%%% 
-%%% The partitioner takes the input list, dispatching each partite element to 
-%%% a pool of worker processes. These worker processes apply the 
-%%% developer-defined function to each partite element. 
-%%% 
-%%% This module supports both the automatic creation of worker processes, and 
-%%% the ability to define the exact number to be used. With the former, the 
-%%% minimal number of workers are created for all inputs. This is given by the 
+%%%
+%%% The partitioner takes the input list, dispatching each partite element to
+%%% a pool of worker processes. These worker processes apply the
+%%% developer-defined function to each partite element.
+%%%
+%%% This module supports both the automatic creation of worker processes, and
+%%% the ability to define the exact number to be used. With the former, the
+%%% minimal number of workers are created for all inputs. This is given by the
 %%% number of elements in the longest list.
-%%% 
+%%%
 %%% @end
 %%%----------------------------------------------------------------------------
 
@@ -31,6 +31,7 @@
 %% Privete exports
 -export([
          loop_auto/4,
+         loop_pull_init/1,
          loop/2
         ]).
 
@@ -38,20 +39,20 @@
 
 
 -spec start(atom(), workflow(), pid()) -> pid().
-%% @doc Starts the recursive partitioning of inputs. 
-%% 
-%% If the number of workers to be used is specified, a list of Pids for those 
-%% worker processes are received through `WorkerPids' and the second clause 
-%% used. Alternatively, a workflow is received as `Workflow' and the first 
-%% clause is used. In the case of the former, the workers have already 
-%% been initialised with their workflows and so the inclusion of the 
-%% `WorkFlow' argument is unneeded in this clause. 
-%% 
-%% The atoms `auto' and `main' are used to determine whether a list of worker 
-%% Pids or a workflow is received. `CombinerPid' specifies the Pid of the 
-%% process that will recompose the partite elements following their 
+%% @doc Starts the recursive partitioning of inputs.
+%%
+%% If the number of workers to be used is specified, a list of Pids for those
+%% worker processes are received through `WorkerPids' and the second clause
+%% used. Alternatively, a workflow is received as `Workflow' and the first
+%% clause is used. In the case of the former, the workers have already
+%% been initialised with their workflows and so the inclusion of the
+%% `WorkFlow' argument is unneeded in this clause.
+%%
+%% The atoms `auto' and `main' are used to determine whether a list of worker
+%% Pids or a workflow is received. `CombinerPid' specifies the Pid of the
+%% process that will recompose the partite elements following their
 %% application to the Workflow.
-%% 
+%%
 %% @todo Wait, can't this atom be gotten rid of? The types are sufficiently different.
 start(auto, WorkFlow, CombinerPid) ->
   sk_tracer:t(75, self(), {?MODULE, start}, [{combiner, CombinerPid}]),
@@ -59,9 +60,11 @@ start(auto, WorkFlow, CombinerPid) ->
 
 -spec start(atom(), workflow(), pos_integer(), pid()) -> pid().
 start(man, WorkFlow, NWorkers, CombinerPid) ->
-  WorkerPids = sk_utils:start_workers(NWorkers, WorkFlow, CombinerPid),
+  WorkerPids = [sk_map_pulling_worker:start(WorkFlow, CombinerPid) || %% TODO started from wrong process
+                 _ <- lists:seq(1,NWorkers)],
   sk_tracer:t(75, self(), {?MODULE, start}, [{combiner, CombinerPid}]),
-  proc_lib:spawn(?MODULE, loop, [decomp_by(), WorkerPids]).
+  %% proc_lib:spawn(?MODULE, loop, [decomp_by(), WorkerPids]).
+  proc_lib:spawn(?MODULE, loop_pull_init, [WorkerPids]).
 
 -spec start( pos_integer(), pos_integer(), workflow(), workflow(), pid()) -> pid().
 start(NCPUWorkers, NGPUWorkers, WorkFlowCPU, WorkFlowGPU, CombinerPid) ->
@@ -71,9 +74,9 @@ start(NCPUWorkers, NGPUWorkers, WorkFlowCPU, WorkFlowGPU, CombinerPid) ->
 
 -spec loop_auto(data_decomp_fun(), workflow(), pid(), [pid()]) -> 'eos'.
 %% @private
-%% @doc Recursively receives inputs as messages, which are decomposed, and the 
-%% resulting messages sent to individual workers. `loop/4' is used in place of 
-%% {@link loop/3} when the number of workers to be used is automatically 
+%% @doc Recursively receives inputs as messages, which are decomposed, and the
+%% resulting messages sent to individual workers. `loop/4' is used in place of
+%% {@link loop/3} when the number of workers to be used is automatically
 %% determined by the total number of partite elements of an input.
 loop_auto(DataPartitionerFun, WorkFlow, CombinerPid, WorkerPids) ->
   receive
@@ -91,8 +94,8 @@ loop_auto(DataPartitionerFun, WorkFlow, CombinerPid, WorkerPids) ->
 
 
 -spec loop(data_decomp_fun(), [pid()]) -> 'eos'.
-%% @doc Recursively receives inputs as messages, which are decomposed, and the 
-%% resulting messages sent to individual workers. `loop/3' is used in place of 
+%% @doc Recursively receives inputs as messages, which are decomposed, and the
+%% resulting messages sent to individual workers. `loop/3' is used in place of
 %% {@link loop/4} when the number of workers is set by the developer.
 loop(DataPartitionerFun, WorkerPids) ->
   receive
@@ -108,9 +111,40 @@ loop(DataPartitionerFun, WorkerPids) ->
     end.
 
 
+loop_pull_init(AllWorkers) ->
+  loop_pull([], [], AllWorkers).
+
+loop_pull(WorkData, WatingWorkers, AllWorkers) ->
+  {WorkData2, WatingWorkers2} =  send_data_to_workers(WorkData, WatingWorkers),
+  receive
+    {give_me_work, Worker} ->
+      loop_pull(WorkData2, [Worker | WatingWorkers2], AllWorkers);
+    {data, Data, Idx} ->
+      NewData = [{data, X, Idx} || X <- Data],
+      loop_pull(WorkData2 ++ NewData, WatingWorkers2, AllWorkers);
+    {system, eos} ->
+      sk_utils:stop_workers(?MODULE, AllWorkers),
+      eos
+  end.
+
+
+send_data_to_workers([Data | Datas], [Worker| Workers]) ->
+  Worker ! Data,
+  send_data_to_workers(Datas, Workers);
+
+%% One of those should be empty
+send_data_to_workers(Data, Workers) ->
+  {Data, Workers}.
+
+
+
+
+
+
+
 -spec decomp_by() -> data_decomp_fun().
-%% @doc Provides the decomposition function and means to split a single input 
-%% into many. This is based on the identity function, as the Map skeleton is 
+%% @doc Provides the decomposition function and means to split a single input
+%% into many. This is based on the identity function, as the Map skeleton is
 %% applied to lists.
 decomp_by() ->
   fun({data, Value, Ids}) ->
@@ -119,12 +153,12 @@ decomp_by() ->
 
 -spec add_workers(pos_integer(), workflow(), pid(), [pid()]) -> [pid()].
 %% @doc Used when the number of workers is not set by the developer.
-%% 
-%% Workers are started if the number needed exceeds the number we already 
-%% have. The total number of workers is derived from the number of partitions 
-%% to which `WorkFlow' will be applied, as given by `NPartitions'. This 
-%% includes 'recycled' workers from previous inputs. Both new and old worker 
-%% processes are returned so that they might be used. Worker processes are 
+%%
+%% Workers are started if the number needed exceeds the number we already
+%% have. The total number of workers is derived from the number of partitions
+%% to which `WorkFlow' will be applied, as given by `NPartitions'. This
+%% includes 'recycled' workers from previous inputs. Both new and old worker
+%% processes are returned so that they might be used. Worker processes are
 %% represented as a list of their Pids under `WorkerPids'.
 add_workers(NPartitions, WorkFlow, CombinerPid, WorkerPids) when NPartitions > length(WorkerPids) ->
   NNewWorkers = NPartitions - length(WorkerPids),
@@ -135,17 +169,17 @@ add_workers(_NPartitions, _WorkFlow, _CombinerPid, WorkerPids) ->
 
 
 -spec dispatch(reference(), pos_integer(), [data_message(),...], [pid()]) -> 'ok'.
-%% @doc Partite elements of input stored in `PartitionMessages' are formatted 
-%% and sent to a worker from `WorkerPids'. The reference argument `Ref' 
-%% ensures that partite elements from different inputs are not incorrectly 
+%% @doc Partite elements of input stored in `PartitionMessages' are formatted
+%% and sent to a worker from `WorkerPids'. The reference argument `Ref'
+%% ensures that partite elements from different inputs are not incorrectly
 %% included.
 dispatch(Ref, NPartitions, PartitionMessages, WorkerPids) ->
   dispatch(Ref, NPartitions, 1, PartitionMessages, WorkerPids).
 
 
 -spec dispatch(reference(), pos_integer(), pos_integer(), [data_message(),...], [pid()]) -> 'ok'.
-%% @doc Inner-function for {@link dispatch/4}. Recursively sends each message 
-%% to a worker, following the addition of references to allow identification 
+%% @doc Inner-function for {@link dispatch/4}. Recursively sends each message
+%% to a worker, following the addition of references to allow identification
 %% and recomposition.
 dispatch(_Ref,_NPartitions, _Idx, [], _) ->
   ok;
---
+++ src/sk_map_pulling_worker.erl
@@ -0,0 +1,36 @@
+-module(sk_map_pulling_worker).
+
+
+-export([
+         start/2,
+         loop/3
+        ]).
+
+
+
+start(Workflow, Combiner) ->
+  Worker = sk_utils:start_worker(Workflow, _SendBack = self()),
+  proc_lib:spawn(?MODULE, loop, [_Source = self(),
+                                 Worker,
+                                 Combiner]).
+
+
+
+loop(Source, Worker, Combiner) ->
+  Source ! {give_me_work, self()},
+  receive
+    {data, _, _} = Data -> %% TODO representation
+      %% todo 
+      Worker ! Data,
+      receive
+        {data, _, _} = OutputData ->
+          Combiner ! OutputData,
+          loop(Source, Worker, Combiner)            
+      end;
+    {system, eos} ->
+      sk_utils:stop_workers(?MODULE, [Worker, Combiner]),
+      eos
+  end.
+       
+    
+  
